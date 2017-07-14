4cb270da0c496e59fa658bb9eb5bee6816452640
nagano564 <nagano564@gmail.com>
2017-07-14 16:19:49 -0400

    master

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,21 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauth_providers => [:facebook]
 
   serialize :checklist, Hash
 
   before_create :setup_checklist
   has_many :user_checklists
 
-  def self.find_for_oauth(auth)
-    find_or_create_by(uid: auth.uid, provider: auth.provider)
-  end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-  
+
   get 'welcome/index'
 
   get 'welcome/about'
